b8f06ad48fc9dcc99753e55483737983d2811356
Christian Vindbæk <christianvf99@gmail.com>
2020-10-21 07:55:24 +0200

    Included logo in navbar

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -4,43 +4,45 @@ import {
     Switch,
     Route,
     useParams,
-  } from "react-router-dom";
-  import { Navbar,Nav,NavDropdown,Form,FormControl,Button } from 'react-bootstrap'
-  import Home from '../homePage/HomeJumbotron';
-  import Contact from '../contactPage/ContactCard';
-  import ContactUs from '../contactPage/ContactCard';
-  import CreateMachine from '../createMachine/CreateMachine';
-  import Login from '../Login/LoginForm';
-
-class BootstrapNavbar extends React.Component{
-
-    render(){
-        return(
+} from "react-router-dom";
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button, Image } from 'react-bootstrap'
+import Home from '../homePage/HomeJumbotron';
+import Contact from '../contactPage/ContactCard';
+import ContactUs from '../contactPage/ContactCard';
+import CreateMachine from '../createMachine/CreateMachine';
+import Login from '../Login/LoginForm';
+//import logo from './logo.png'
+const logo = require('./logoWhite.svg');
+class BootstrapNavbar extends React.Component {
+    render() {
+        return (
             <div>
                 <div className="row">
                     <div className="col-md-12">
                         <Router>
                             <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
-                                <Navbar.Brand href="#home">React Bootstrap Navbar</Navbar.Brand>
+                                <Navbar.Brand href="#home">
+                                    <img src={logo} height="50fr" ></img> 
+                                </Navbar.Brand>
                                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                                 <Navbar.Collapse id="basic-navbar-nav">
                                     <Nav className="mr-auto">
-                                    <Nav.Link href="/">Home</Nav.Link>
-                                    <Nav.Link href="/contact">Contact Us</Nav.Link>
-                                    <Nav.Link href="/contact-us">About Us</Nav.Link>
-                                    <Nav.Link href="/login">Login</Nav.Link>
-                                    <Nav.Link href="/createMachine">CreateMachine</Nav.Link>
-                                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                        <NavDropdown.Divider />
-                                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                                    </NavDropdown>
+                                        <Nav.Link href="/">Home</Nav.Link>
+                                        <Nav.Link href="/contact">Contact Us</Nav.Link>
+                                        <Nav.Link href="/contact-us">About Us</Nav.Link>
+                                        <Nav.Link href="/login">Login</Nav.Link>
+                                        <Nav.Link href="/createMachine">CreateMachine</Nav.Link>
+                                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                            <NavDropdown.Divider />
+                                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                                        </NavDropdown>
                                     </Nav>
                                     <Form inline>
-                                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                                    <Button variant="outline-success">Search</Button>
+                                        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                                        <Button variant="outline-success">Search</Button>
                                     </Form>
                                 </Navbar.Collapse>
                             </Navbar>
@@ -61,9 +63,9 @@ class BootstrapNavbar extends React.Component{
 
                                 <Route path="/contact-us">
                                     <ContactUs />
-                                    </Route>
+                                </Route>
 
-                                    <Route exact path="/createMachine">
+                                <Route exact path="/createMachine">
                                     <CreateMachine />
 
                                 </Route>
@@ -72,7 +74,7 @@ class BootstrapNavbar extends React.Component{
                     </div>
                 </div>
             </div>
-        )  
+        )
     }
 }
 
